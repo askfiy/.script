bb907a841932dd8463b76ba7633922e30f6efeec
askfiy <c2323182108@gmail.com>
2022-04-20 23:07:05 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+Some script files defined by myself.
+
+```
+$ cd ~
+$ git clone https://github.com/askfiy/.script
+```
+
+- ./dwm/autostart.sh: you may need to make some modifications to this file
+- ./dwm/background/: put your favorite wallpapers here
---
+++ dwm/autostart.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+xrandr --output HDMI-1 --mode 2560x1080 --rate 75 --output eDP-1 --off
+
+function start_running(){
+  STATUS=$(ps -C $1 --no-header | wc -l)
+  echo $1 $STATUS
+  if [ $STATUS -eq 0 ]; then
+    $1 &
+  fi
+}
+
+start_running fcitx5
+start_running slstatus
+start_running flameshot
+start_running /opt/clash-for-windows-bin/cfw
+start_running $HOME/Applications/picgo_7b2f2e9b0915e6e5470619db5c8ba948.AppImage
+
+./wallpaper.sh &
